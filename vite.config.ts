eec48a0a91ea786bb6e74136d000249203dfc037
SilentCrouwd <chris.buchholz.lauenau@googlemail.com>
2026-07-28 17:59:03 +0200

    feat: add impressum page and update index layout with pricing section

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   base: "/cb-codes",
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: new URL("./index.html", import.meta.url).pathname,
+        impressum: new URL("./impressum.html", import.meta.url).pathname,
+      },
+    },
+  },
 });
